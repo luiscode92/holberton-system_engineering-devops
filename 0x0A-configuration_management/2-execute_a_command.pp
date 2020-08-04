214a2331c11108be2c5abb023d8d54cf386fb602
Luis Sanjuan <luiscode92@gmail.com>
2020-08-03 23:09:08 -0500

    linter fixed

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #using puppet, create a manifest that kills a process named killmenow
 
 exec { 'kill a process' :
-  command  => 'pkill killmenow',
-  path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+  command => 'pkill killmenow',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
