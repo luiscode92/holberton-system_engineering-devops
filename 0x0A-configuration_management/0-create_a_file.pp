57372a18c20067be6bbe0092af4a4ba4dee76d4f
Luis Sanjuan <luiscode92@gmail.com>
2020-08-03 21:36:43 -0500

    upidate configuratioan managment

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#using puppet to create a file.
+
+file { '/tmp/holberton' :
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
