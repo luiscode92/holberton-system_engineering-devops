b87116c09103742e2e2d41f11248f37904b1ce78
Luis Sanjuan <luiscode92@gmail.com>
2020-08-04 22:18:56 -0500

    add passwrd auth

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#configure SSH client to use private keY ~/ssh/holberton adn refuse to authenticate usign pswrd
+include stdlib
+
+file_line { 'Use private key':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'Refuse password',
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
