7f983da1381898e5a1d5af9a0388d066734d4f8d
Luis Sanjuan <luiscode92@gmail.com>
2020-09-14 18:24:05 -0500

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
     data = resp.json()
 
     with open('{}.csv'.format(argv[1]), mode='w') as File:
-        writer = csv.writer(File, delimeter=',' quotechar='"', quoting=csv.QUOTE_ALL)
-
+        writer = csv.writer(File, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         for task in data:
             writer.writerow([argv[1], username, task.get('completed'), task.get('title')])
