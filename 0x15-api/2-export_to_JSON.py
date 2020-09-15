a8486cf71485101d8d21a2623d3327be851d3b03
Luis Sanjuan <luiscode92@gmail.com>
2020-09-14 19:26:45 -0500

    update pep8

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ script that given a emplyee ID, return info about todo list
 """
-import requests
-from sys import argv
-import json
+if __name__ == __name__:
+    import requests
+    from sys import argv
+    import json
 
-
-if __name__ == '__main__':
     resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
     username = resp.json().get('username')
     resp = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
