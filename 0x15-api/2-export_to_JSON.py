6a9d080e21e172e003932c6dd64e12c0e0575fae
Luis Sanjuan <luiscode92@gmail.com>
2020-09-14 19:14:34 -0500

    update- export json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that given a emplyee ID, return info about todo list
+"""
+import requests
+from sys import argv
+import json
+
+
+if __name__ == '__main__':
+    resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    username = resp.json().get('username')
+    resp = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+    data = resp.json()
+
+    export = {}
+    export['{}'.format(argv[1])] = []
+    for task in data:
+        export['{}'.format(argv[1])].append({
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': username
+        })
+    with open('{}.json'.format(argv[1]), mode='w') as file:
+        json.dump(export, file)
