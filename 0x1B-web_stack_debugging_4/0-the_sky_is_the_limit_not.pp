5604afedcbd610e85b2ee4924f074f2023a98d91
Luis Sanjuan <luiscode92@gmail.com>
2020-10-08 15:40:59 -0500

    update puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Increase the user limit from 15 to 4096
 exec { 'limit':
-   command => "sed -i -e 's/15/4096/g /etc/default/nginx; service nginx restart",
-   path    => ['/bin', '/usr/bin', '/usr/sbin']
+	  command => "sed -i -e 's/15/4096/g' /etc/default/nginx; service nginx restart",
+		    path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
