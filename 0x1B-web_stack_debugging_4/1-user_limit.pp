d6e514cea1fcb1f769df9f634879b8cca9823e0b
Luis Sanjuan <luiscode92@gmail.com>
2020-10-08 15:43:26 -0500

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,10 @@
 # Allow login with holberton user and open a file without any error message
 exec { 'hard':
-  command => "sed -i -e 's/5/500/g' /etc/security/limits.conf",
-  path    => '/bin'
+	  command => "sed -i -e 's/5/500/g' /etc/security/limits.conf",
+		    path    => '/bin'
 }
 
 exec { 'soft':
-  command => "sed -i -e 's/4/500/g' /etc/security/limits..conf",
-  path    => '/bin'
+	  command => "sed -i -e 's/4/500/g' /etc/security/limits.conf",
+		    path    => '/bin'
+}
